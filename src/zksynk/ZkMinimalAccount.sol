c5c53eaa3857dc2c1582c351b2d1af89bbeacec8
fraolb <fraolbereket@gmail.com>
2024-12-18 11:08:42 +0300

    import packages

--- src/zksynk/ZkMinimalAccount.sol
+++ src/zksynk/ZkMinimalAccount.sol
@@ -1,6 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+// zkSync Era Imports
+import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+import {Utils} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
+
+// OZ Imports
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Lifecycle of a type 113 (0x71) transaction
+ * msg.sender is the bootloader system contract
+ *
+ * Phase 1 Validation
+ * 1. The user sends the transaction to the "zkSync API client" (sort of a "light node")
+ * 2. The zkSync API client checks to see the the nonce is unique by querying the NonceHolder system contract
+ * 3. The zkSync API client calls validateTransaction, which MUST update the nonce
+ * 4. The zkSync API client checks the nonce is updated
+ * 5. The zkSync API client calls payForTransaction, or prepareForPaymaster & validateAndPayForPaymasterTransaction
+ * 6. The zkSync API client verifies that the bootloader gets paid
+ *
+ * Phase 2 Execution
+ * 7. The zkSync API client passes the validated transaction to the main node / sequencer (as of today, they are the same)
+ * 8. The main node calls executeTransaction
+ * 9. If a paymaster was used, the postTransaction is called
+ */
 contract ZkMinimalAccount {
     error ZkMinimalAccount__NotEnoughBalance();
     error ZkMinimalAccount__NotFromBootLoader();
