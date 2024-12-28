31321e3585a0da66cec693d36416f27e709b0e7b
fraolb <fraolbereket@gmail.com>
2024-12-28 13:20:58 +0300

    test zk abstraction

---
+++ test/zksync/ZkMinimalAccountTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ZkMinimalAccount} from "src/zksync/ZkMinimalAccount.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract ZkMinimalAccountTest is Test {}
